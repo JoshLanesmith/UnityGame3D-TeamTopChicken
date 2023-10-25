3f14f85537a97ce4233de551a0e75178a3c2fe67
김기훈 <rlgnsgh123@naver.com>
2023-10-25 18:42:35 -0400

    ClearCounter&KitchenObject

--- TopChickenKitchenChaos/Assets/Scripts/ClearCounter.cs
+++ TopChickenKitchenChaos/Assets/Scripts/ClearCounter.cs
@@ -7,16 +7,42 @@ public class ClearCounter : MonoBehaviour
 {
     // Reference
     [SerializeField] private KitchenObjectSO kitchenObjectSO; 
-    [SerializeField] private Transform counterTopPoint; 
+    [SerializeField] private Transform counterTopPoint;
+    [SerializeField] private ClearCounter secondClearCounter;
+    [SerializeField] private bool testing;
+
+    private KitchenObject kitchenObject;
+
+    private void Update()
+    {
+        if (testing && Input.GetKeyDown(KeyCode.T))
+        {
+            if (kitchenObject != null)
+            {
+                kitchenObject.SetClearCounter(secondClearCounter);
+            }
+
+        }
+    }
     public void Interact()
     {
-        Debug.Log("Interact!");
+        //Debug.Log("Interact!");
         //Transform and locate in the top of the table
-        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint );
-        kitchenObjectTransform.localPosition = Vector3.zero;
+        if (kitchenObject == null)
+        {
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+            kitchenObjectTransform.localPosition = Vector3.zero;
+            kitchenObject = kitchenObjectTransform.GetComponent<KitchenObject>();
+            kitchenObject.SetClearCounter(this);
+        }
+        else
+        {
+            Debug.Log(kitchenObject.GetClearCounter());
+        }
+       
 
         // Show interacted and respond kitchenObject
-        Debug.Log(kitchenObjectTransform.GetComponent<KitchenObject>().GetKitchenObjectSO().objectName);
+        //Debug.Log(kitchenObjectTransform.GetComponent<KitchenObject>().GetKitchenObjectSO().objectName);
         //the type we resopond accoring to prefabs
     }
 }
